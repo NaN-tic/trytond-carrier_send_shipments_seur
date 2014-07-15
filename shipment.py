f8d6a9552a2d65e4f27c3775592b8a4d1364902a
resteve <resteve@zikzakmedia.com>
2014-07-15 12:33:08 +0200

    Send Shipment: send date and employee

--- shipment.py
+++ shipment.py
@@ -129,6 +129,8 @@ class ShipmentOut:
                         'carrier_tracking_ref': reference,
                         'carrier_service': service,
                         'carrier_delivery': True,
+                        'carrier_send_date': ShipmentOut.get_carrier_date(),
+                        'carrier_send_employee': ShipmentOut.get_carrier_employee() or None,
                         })
                     logging.getLogger('seur').info(
                         'Send shipment %s' % (shipment.code))
