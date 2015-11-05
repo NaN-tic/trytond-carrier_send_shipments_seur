9ec185e0d6a28cba5b8f8cc3ff3af8d904b5657e
resteve <resteve@zikzakmedia.com>
2015-11-05 09:00:31 +0100

    Label convert unicode to encode utf8 (send + label)

--- shipment.py
+++ shipment.py
@@ -195,7 +195,7 @@ class ShipmentOut:
                         with tempfile.NamedTemporaryFile(
                                 prefix='%s-seur-%s-' % (dbname, reference),
                                 suffix='.txt', delete=False) as temp:
-                            temp.write(label)
+                            temp.write(label.encode('utf-8'))
                     logger.info('Generated tmp label %s' % (temp.name))
                     temp.close()
                     labels.append(temp.name)
