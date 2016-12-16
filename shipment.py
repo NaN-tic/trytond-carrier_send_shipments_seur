5c367217f3cfcfe9e4b9f006111fc5e3aee491ae
Raimon Esteve <raimon@nan-tic.com>
2016-12-16 16:58:31 +0100

    Seur Offline

--- shipment.py
+++ shipment.py
@@ -5,14 +5,21 @@ from trytond.pool import Pool, PoolMeta
 from trytond.transaction import Transaction
 from seur.picking import Picking
 from trytond.modules.carrier_send_shipments.tools import unaccent, unspaces
+from trytond.modules.carrier_send_shipments_seur.tools import set_seur_reference, \
+    seurbarcode
 from base64 import decodestring
+import os
 import logging
 import tempfile
+import genshi
 
 __all__ = ['ShipmentOut']
 __metaclass__ = PoolMeta
 
 logger = logging.getLogger(__name__)
+offline_loader = genshi.template.TemplateLoader(
+    os.path.join(os.path.dirname(__file__), 'template'),
+    auto_reload=True)
 
 
 class ShipmentOut:
@@ -27,9 +34,11 @@ class ShipmentOut:
             'seur_not_price': 'Shipment "%(name)s" not have price and send '
                 'cashondelivery',
             'seur_error_zip': 'Seur not accept zip "%(zip)s"',
-            'seur_not_send': 'Not send shipment %(name)s',
-            'seur_not_send_error': 'Not send shipment %(name)s. %(error)s',
+            'seur_not_send': 'Not send shipment "%(name)s"',
+            'seur_not_send_error': 'Not send shipment "%(name)s". %(error)s',
             'seur_not_label': 'Not available "%(name)s" label from Seur',
+            'seur_reference_int': 'Seur reference is not an integer. Please, '
+                'check the seur sequence',
             })
 
     @staticmethod
@@ -43,7 +52,10 @@ class ShipmentOut:
         :param weight: bol
         Return data
         '''
-        Uom = Pool().get('product.uom')
+        pool = Pool()
+        Uom = pool.get('product.uom')
+        Date = pool.get('ir.date')
+        SeurZip = pool.get('carrier.api.seur.zip')
 
         if api.reference_origin and hasattr(shipment, 'origin'):
             code = shipment.origin and shipment.origin.rec_name or shipment.code
@@ -58,11 +70,30 @@ class ShipmentOut:
         if not packages or packages == 0:
             packages = 1
 
+        if shipment.warehouse.address:
+            waddress = shipment.warehouse.address
+        else:
+            waddress = api.company.party.addresses[0]
+
+        warehouse_street = waddress.street
+        warehouse_city = unaccent(waddress.city)
+        warehouse_zip = waddress.zip
+        warehouse_country_code = waddress.country.code
+
         customer_name = shipment.delivery_address.name or shipment.customer.name
         customer_street = shipment.delivery_address.street
         customer_city = unaccent(shipment.delivery_address.city)
         customer_zip = shipment.delivery_address.zip
         customer_country_code = shipment.delivery_address.country.code
+
+        seur_warehouse_zips = SeurZip.search([
+            ('codpos_zip', '=', warehouse_zip),
+            ('codpos_country', '=', warehouse_country_code),
+            ], limit=1)
+        seur_customer_zips = SeurZip.search([
+            ('codpos_zip', '=', customer_zip),
+            ('codpos_country', '=', customer_country_code),
+            ], limit=1)
 
         notes = '%(notes)s' \
             '%(name)s. %(street)s. %(zip)s %(city)s - %(country)s\n' % {
@@ -75,8 +106,17 @@ class ShipmentOut:
                 }
 
         data = {}
+        data['date'] = Date.today().strftime('%d/%m/%y')
+        data['company_name'] = api.company.party.name
+        data['company_street'] = warehouse_street
+        data['company_zip'] = seur_warehouse_zips[0].codpos_code \
+            if seur_warehouse_zips else warehouse_zip
+        data['company_city'] = seur_warehouse_zips[0].codpos_city \
+            if seur_warehouse_zips else warehouse_city
+
         data['servicio'] = str(service.code)
-        data['product'] = '2'
+        # international: service 77, product 104
+        data['product'] = '104' if service.code == '77' else '2'
         data['total_bultos'] = packages
         data['observaciones'] = notes
         data['referencia_expedicion'] = code
@@ -89,19 +129,21 @@ class ShipmentOut:
             data['clave_reembolso'] = ' '
             data['valor_reembolso'] = '0'
 
+        sweight = 1.0
         if weight and hasattr(shipment, 'weight_func'):
-            weight = shipment.weight_func
-            if weight == 0:
-                weight = 1
+            sweight = shipment.weight_func
+            if sweight == 0 or sweight == 0.0:
+                sweight = 1.0
             if api.weight_api_unit:
                 if shipment.weight_uom:
-                    weight = Uom.compute_qty(
+                    sweight = Uom.compute_qty(
                         shipment.weight_uom, weight, api.weight_api_unit)
                 elif api.weight_unit:
-                    weight = Uom.compute_qty(
+                    sweight = Uom.compute_qty(
                         api.weight_unit, weight, api.weight_api_unit)
-            data['total_kilos'] = str(weight)
-            data['peso_bulto'] = str(weight)
+
+        data['total_kilos'] = str(sweight)
+        data['peso_bulto'] = str(sweight)
 
         data['cliente_nombre'] = unaccent(customer_name)
         data['cliente_direccion'] = unaccent(customer_street)
@@ -111,30 +153,38 @@ class ShipmentOut:
         #~ data['cliente_escalera'] = 'A'
         #~ data['cliente_piso'] = '3'
         #~ data['cliente_puerta'] = '2'
-        data['cliente_poblacion'] = unaccent(customer_city)
-        data['cliente_cpostal'] = customer_zip
+        data['cliente_cpostal'] = seur_customer_zips[0].coddest_name \
+            if seur_customer_zips else customer_zip
+        data['cliente_poblacion'] = customer_city
         data['cliente_pais'] = customer_country_code
         if shipment.customer.email:
             if shipment.delivery_address.email:
                 data['cliente_email'] = shipment.delivery_address.email
             else:
                 data['cliente_email'] = shipment.customer.email
-        data['cliente_telefono'] = unspaces(shipment.get_phone_shipment_out(shipment))
+        data['cliente_telefono'] = unspaces(
+            shipment.get_phone_shipment_out(shipment))
+        data['sms_consignatario'] = unspaces(
+            shipment.get_phone_shipment_out(shipment, phone=False))
         data['cliente_atencion'] = unaccent(customer_name)
         data['aviso_preaviso'] = 'S' if api.seur_aviso_preaviso else 'N'
         data['aviso_reparto'] = 'S' if api.seur_aviso_reparto else 'N'
         data['aviso_email'] = 'S' if api.seur_aviso_email else 'N'
         data['aviso_sms'] = 'S' if api.seur_aviso_sms else 'N'
+        data['id_mercancia'] = '400' # TODO fixed ID mercancia
         return data
 
     @classmethod
-    def send_seur(self, api, shipments):
-        '''
-        Send shipments out to seur
-        :param api: obj
-        :param shipments: list
-        Return references, labels, errors
-        '''
+    def send_seur(cls, api, shipments):
+        'Send shipments out to seur'
+        if api.seur_offline:
+            return cls.send_seur_offline(api, shipments)
+        else:
+            return cls.send_seur_api(api, shipments)
+
+    @classmethod
+    def send_seur_api(cls, api, shipments):
+        'Send shipments out to seur'
         pool = Pool()
         CarrierApi = pool.get('carrier.api')
         ShipmentOut = pool.get('stock.shipment.out')
@@ -154,13 +204,13 @@ class ShipmentOut:
             for shipment in shipments:
                 service = shipment.carrier_service or shipment.carrier.service or default_service
                 if not service:
-                    message = self.raise_user_error('seur_add_services', {},
+                    message = cls.raise_user_error('seur_add_services', {},
                         raise_exception=False)
                     errors.append(message)
                     continue
 
                 if not shipment.delivery_address.country:
-                    message = self.raise_user_error('seur_not_country', {},
+                    message = cls.raise_user_error('seur_not_country', {},
                         raise_exception=False)
                     errors.append(message)
                     continue
@@ -169,17 +219,17 @@ class ShipmentOut:
                 if shipment.carrier_cashondelivery:
                     price = ShipmentOut.get_price_ondelivery_shipment_out(shipment)
                     if not price:
-                        message = self.raise_user_error('seur_not_price', {
+                        message = cls.raise_user_error('seur_not_price', {
                                 'name': shipment.rec_name,
                                 }, raise_exception=False)
                         errors.append(message)
                         continue
 
-                data = self.seur_picking_data(api, shipment, service, price, api.weight)
+                data = cls.seur_picking_data(api, shipment, service, price, api.weight)
                 reference, label, error = picking_api.create(data)
 
                 if reference:
-                    self.write([shipment], {
+                    cls.write([shipment], {
                         'carrier_tracking_ref': reference,
                         'carrier_service': service,
                         'carrier_delivery': True,
@@ -207,19 +257,108 @@ class ShipmentOut:
                     temp.close()
                     labels.append(temp.name)
                 else:
-                    message = self.raise_user_error('seur_not_label', {
+                    message = cls.raise_user_error('seur_not_label', {
                             'name': shipment.rec_name,
                             }, raise_exception=False)
                     errors.append(message)
                     logger.error(message)
 
                 if error:
-                    message = self.raise_user_error('seur_not_send_error', {
+                    message = cls.raise_user_error('seur_not_send_error', {
                             'name': shipment.rec_name,
                             'error': error,
                             }, raise_exception=False)
                     logger.error(message)
                     errors.append(message)
+
+        return references, labels, errors
+
+    @classmethod
+    def send_seur_offline(cls, api, shipments):
+        'Send Seur Offline'
+        pool = Pool()
+        SeurOffline = pool.get('carrier.api.seur.offline')
+        ShipmentOut = pool.get('stock.shipment.out')
+        Sequence = pool.get('ir.sequence')
+        CarrierApi = pool.get('carrier.api')
+
+        # XML data will be created when send Seur email
+
+        tmpl = offline_loader.load('offline-label.zpl',
+            cls=genshi.template.text.NewTextTemplate)
+
+        dbname = Transaction().cursor.dbname
+        min_ref = api.seur_minimum_reference
+        max_ref = api.seur_maximun_reference
+        default_service = CarrierApi.get_default_carrier_service(api)
+        sequence_id = api.seur_reference.id
+
+        references = []
+        labels = []
+        errors = []
+
+        to_create = []
+        to_write = []
+        for shipment in shipments:
+            price = None
+            if shipment.carrier_cashondelivery:
+                price = ShipmentOut.get_price_ondelivery_shipment_out(shipment)
+
+            service = shipment.carrier_service or shipment.carrier.service \
+                or default_service
+
+            vals = cls.seur_picking_data(api, shipment, service, price, api.weight)
+
+            if vals['clave_portes'] == 'D':
+                vals['clave_portes'] = 'P.Debidos'
+            else:
+                # clave_protes == F
+                vals['clave_portes'] = 'P.Pagados'
+
+            seur_references = []
+            for i in range(0, vals['total_bultos']):
+                try:
+                    reference = int(Sequence.get_id(sequence_id))
+                except:
+                    cls.raise_user_error('seur_reference_int')
+                seur_reference = str(set_seur_reference(min_ref, max_ref, reference))
+                seur_references.append(seur_reference)
+
+                barcode = seurbarcode(
+                    from_zip=shipment.warehouse.address.zip,
+                    to_zip=vals['cliente_cpostal'],
+                    reference=seur_reference,
+                    transport=1) # TODO transport type is fixed to 1
+                vals['barcode'] = barcode
+                vals['barcode_compact'] = barcode.replace (' ', '')
+                vals['bulto'] = i + 1
+
+                zpl = tmpl.generate(**vals).render()
+                with tempfile.NamedTemporaryFile(
+                        prefix='%s-seur-%s-' % (dbname, seur_reference),
+                        suffix='.zpl', delete=False) as temp:
+                    temp.write(zpl.encode('utf-8'))
+
+                logger.info(
+                    'Generated tmp label %s' % (temp.name))
+                labels.append(temp.name)
+                temp.close()
+
+            to_create.append({
+                'api': api,
+                'shipment': shipment,
+                'state': 'draft',
+                })
+            to_write.extend(([shipment], {
+                'carrier_tracking_ref': ','.join(seur_references),
+                }))
+            references.extend(seur_references)
+
+        if to_write:
+            ShipmentOut.write(*to_write)
+        if to_create:
+            with Transaction().set_user(0):
+                SeurOffline.create(to_create)
 
         return references, labels, errors
 
---
+++ template/offline-label.zpl
@@ -0,0 +1,38 @@
+^XA^XFSEURCPT1.001^FS
+^FN1^FD*B2C/ESTD^FS
+^FN2^FD${date}^FS
+^FN3^FD${company_city}^FS
+^FN4^FD${cliente_poblacion}^FS
+^FN5^FD${cliente_poblacion}^FS
+^FN6^FDR^FS
+^FN7^FD ^FS
+^FN8^FD ^FS
+^FN9^FD ^FS
+^FN10^FD${clave_portes}^FS
+^FN11^FD  ^FS
+^FN12^FD${company_name}^FS
+^FN13^FD${company_street}^FS
+^FN14^FD^FS
+^FN15^FD${company_zip} ${company_city}^FS
+^FN16^FD${cliente_nombre}^FS
+^FN17^FD${cliente_direccion}^FS
+^FN18^FD^FS
+^FN19^FD${cliente_cpostal} ${cliente_poblacion}^FS
+^FN20^FD${cliente_telefono}^FS
+^FN21^FD^FS
+^FN22^FD${cliente_atencion}^FS
+^FN23^FD,^FS
+^FN24^FD${ref_bulto}^FS
+^FN25^FDA^FS
+^FN26^FD^FS
+^FN27^FD,^FS
+^FN28^FD^FS
+^FN29^FD^FS
+^FN30^FD${barcode_compact}^FS
+^FN31^FD${barcode}^FS
+^FN32^FD${bulto}/${total_bultos}^FS
+^FN33^FD${peso_bulto}^FS
+^FN34^FD^FS
+^FN35^FD^FS
+^FN36^FD^FS
+^XZ
